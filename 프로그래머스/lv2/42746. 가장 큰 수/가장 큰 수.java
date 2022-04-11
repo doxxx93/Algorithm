42395aad343efb85623bc07203a2ef1fe33a4f8e
D <51396905+doxxx93@users.noreply.github.com>
2022-04-11 23:38:15 +0900

    [level 2] Title: 가장 큰 수, Time: 2.42 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -4,11 +4,11 @@ class Solution {
     public String solution(int[] numbers) {
         String answer = "";
         String[] str = new String[numbers.length];
-        
+
         for(int i = 0; i < numbers.length; i++){
             str[i] = String.valueOf(numbers[i]);
         }
-        
+
         Arrays.sort(str, new Comparator<String>() {
 
             public int compare(String a, String b) {
@@ -16,14 +16,14 @@ class Solution {
                 //오름차순 정렬 (o1+o2).compareTo(o1+o2);
             }
         });
-        
+
         //0값이 중복일경우 ex){0,0,0}
         //답이 000이 나오면 안되므로 첫번째값이 0이면 0을 리턴
         if (str[0].equals("0")) return "0";
-        
+
         //0이 아니면 문자열을 더해준다.
         for(String s: str) answer += s;
- 
+
         return answer;
     }
 }
