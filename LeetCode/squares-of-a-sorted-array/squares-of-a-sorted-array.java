f356f71c27de0a46f2422cbb7b9e2d09c3bb0420
D <51396905+doxxx93@users.noreply.github.com>
2022-04-11 22:44:04 +0900

    Time: 18 ms, Memory: 57 MB - LeetHub

---
+++ LeetCode/squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        return Arrays.stream(nums)
+                .map(num -> num * num)
+                .sorted()
+                .toArray();
+    }
+}
