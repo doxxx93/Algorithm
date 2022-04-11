5cd09f77611eb163cb8f5c44ef1b5443a3c7ac4a
D <51396905+doxxx93@users.noreply.github.com>
2022-04-11 23:03:06 +0900

    Time: 1 ms, Memory: 42.5 MB - LeetHub

---
+++ LeetCode/duplicate-zeros/duplicate-zeros.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int zeroCount = 0;
+        
+        for (int a : arr) {
+            if (a == 0) zeroCount++;
+        }
+        
+        int n = arr.length;
+        
+        for (int i = n-1; i >= 0; i--) {
+            int j = i + zeroCount;
+            
+            if (arr[i] == 0) {
+                if (j < n) arr[j] = 0;
+                zeroCount--;
+                j--;
+            }
+            
+            if (j < n) arr[j] = arr[i];
+        }
+    }
+}
