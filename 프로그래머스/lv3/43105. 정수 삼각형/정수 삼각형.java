6e2213728d5ba471e2464c4c7b487c856eccae7a
D <51396905+doxxx93@users.noreply.github.com>
2022-04-11 23:36:31 +0900

    [level 3] Title: 정수 삼각형, Time: 7.17 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,25 +1,42 @@
 class Solution {
-    public int solution(int[][] triangle) {
 
-        int[][] dp = new int[triangle.length][triangle.length];
-        dp[0][0] = triangle[0][0];
+    private int[][] value;
+    private Integer[][] dp;
+    //dp[a][b] = 어떤걸 의미하는가
 
-        for (int i=1; i< triangle.length; i++) {
+    public int dfs(int x, int y){
+        //if(x < 0|| y < 0) return 0;
+        if(dp[x][y] == null){
 
-            dp[i][0] = triangle[i][0] + dp[i - 1][0];
-
-            for (int j=1; j<i+1; j++) {
-                dp[i][j] = triangle[i][j] + Math.max(dp[i -1][j - 1], dp[i -1][j]);
+            if(y == 0){
+                dp[x][y] = value[x][y] + dfs(x-1, y);
+            }else if(y == x){
+                dp[x][y] = value[x][y] + dfs(x-1, y-1);
+            }else{
+                dp[x][y] = value[x][y] +Math.max(dfs(x-1, y-1),dfs(x-1, y));
             }
         }
+        return dp[x][y];
+    }
 
-        int max = 0;
-        for (int i=0; i<dp[dp.length - 1].length; i++) {
-            max = Math.max(dp[dp.length - 1][i], max);
+
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        // 메모이제이션을 사용해서 문제를 해결
+        // 각각의 위치에서 최대합은 항상 일정하고 그 정보를 가지고 그다음 최대합을 구하는 식으로 진행
+        // top-down
+        this.value = triangle;
+        dp = new Integer[triangle.length][triangle.length];
+        //init setting
+        dp[0][0] = value[0][0];
+        for(int i = 0; i<value.length; i++){
+            dfs(value.length - 1, i);
         }
 
-
-        int answer = max;
-        return answer;
+        int max = -1;
+        for(int i = 0;i<value.length; i++){
+            max = Math.max(dp[value.length-1][i], max);
+        }
+        return max;
     }
 }
