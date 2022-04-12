f1ca933f9f6f9315f240fe6b674fc5e99788350b
D <51396905+doxxx93@users.noreply.github.com>
2022-04-12 09:37:08 +0900

    [level 2] Title: 위장, Time: 0.05 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.java
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -1,19 +1,22 @@
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.Map;
 
 class Solution {
+
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> counts = new HashMap<>();
+
         for (String[] clothe : clothes) {
             String type = clothe[1];
-            map.put(type, map.getOrDefault(type, 0) + 1);
+            counts.put(type, counts.getOrDefault(type, 0) + 1);
         }
-        Iterator<Integer> it = map.values().iterator();
-        int answer = 1;
-        while (it.hasNext()) {
-            answer *= it.next().intValue() + 1;
-        }
-        return answer - 1;
-    }
 
+        int answer = 1;
+        for (Integer count : counts.values()) {
+            answer *= count + 1;
+        }
+        answer -= 1;
+
+        return answer;
+    }
 }
