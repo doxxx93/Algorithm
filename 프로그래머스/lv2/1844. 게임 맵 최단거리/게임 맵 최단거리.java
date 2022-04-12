3c749b275f40055d3877674fc6b098196bbf3fd4
D <51396905+doxxx93@users.noreply.github.com>
2022-04-12 10:42:22 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.00 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,49 +3,70 @@ import java.util.Queue;
 
 class Solution {
 
-    int[][] maps, distance;
-    int height, width;
-
     public int solution(int[][] maps) {
-        this.maps = maps;
-        height = maps.length;
-        width = maps[0].length;
-        distance = new int[height][width];
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                distance[i][j] = -1;
-            }
-        }
-        bfs();
-        return distance[height - 1][width - 1];
-    }
+        // BFS: Queue
+        // 현재 위치를 큐에 적용할 것
+        Queue<Position> queue = new LinkedList<>();
 
-    void bfs() {
-        int[] dy = {1, -1, 0, 0}, dx = {0, 0, 1, -1};
-        distance[0][0] = 1;
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(0, 0));
-        while (!q.isEmpty()) {
-            Node curr = q.poll();
-            for (int d = 0; d < 4; d++) {
-                Node next = new Node(curr.y + dy[d], curr.x + dx[d]);
-                if (0 <= next.y && next.y < maps.length && 0 <= next.x && next.x < maps[0].length) {
-                    if (distance[next.y][next.x] == -1 && maps[next.y][next.x] == 1) {
-                        distance[next.y][next.x] = distance[curr.y][curr.x] + 1;
-                        q.add(next);
-                    }
+        int mapHeight = maps.length;
+        int mapWidth = maps[0].length;
+        int[][] count = new int[mapHeight][mapWidth];
+        boolean[][] visited = new boolean[mapHeight][mapWidth];
+
+        queue.offer(new Position(0, 0));
+        count[0][0] = 1;
+        visited[0][0] = true;
+
+        while (!queue.isEmpty()) {
+            Position current = queue.poll();
+
+            int currentCount = count[current.y][current.x];
+            // 4가지 경우
+            final int[][] moving = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+            for (int i = 0; i < moving.length; i++) {
+                Position moved = new Position(current.x + moving[i][0], current.y + moving[i][1]);
+                if (!moved.isValid(mapWidth, mapHeight)) {
+                    continue;
                 }
+                if (visited[moved.y][moved.x]) {
+                    continue;
+                }
+                if (maps[moved.y][moved.x] == 0) {
+                    continue;
+                }
+
+                count[moved.y][moved.x] = currentCount + 1;
+                visited[moved.y][moved.x] = true;
+                queue.offer(moved);
             }
         }
+        int answer = count[mapHeight - 1][mapWidth - 1];
+        if (answer == 0) {
+            return -1;
+        }
+
+        return answer;
     }
-}
 
-class Node {
+    class Position {
 
-    int y, x;
+        int x, y;
 
-    Node(int y, int x) {
-        this.y = y;
-        this.x = x;
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        boolean isValid(int width, int height) {
+            if (x < 0 || x >= width) {
+                return false;
+            }
+            if (y < 0 || y >= height) {
+                return false;
+            }
+            return true;
+
+        }
+
     }
 }
