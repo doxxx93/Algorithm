e1148bd657f925302426322e8e6b4315af278b9e
D <51396905+doxxx93@users.noreply.github.com>
2022-04-12 11:52:03 +0900

    [level 3] Title: 정수 삼각형, Time: 10.50 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,42 +1,28 @@
 class Solution {
 
-    private int[][] value;
-    private Integer[][] dp;
-    //dp[a][b] = 어떤걸 의미하는가
-
-    public int dfs(int x, int y){
-        //if(x < 0|| y < 0) return 0;
-        if(dp[x][y] == null){
-
-            if(y == 0){
-                dp[x][y] = value[x][y] + dfs(x-1, y);
-            }else if(y == x){
-                dp[x][y] = value[x][y] + dfs(x-1, y-1);
-            }else{
-                dp[x][y] = value[x][y] +Math.max(dfs(x-1, y-1),dfs(x-1, y));
-            }
-        }
-        return dp[x][y];
-    }
-
-
     public int solution(int[][] triangle) {
         int answer = 0;
-        // 메모이제이션을 사용해서 문제를 해결
-        // 각각의 위치에서 최대합은 항상 일정하고 그 정보를 가지고 그다음 최대합을 구하는 식으로 진행
-        // top-down
-        this.value = triangle;
-        dp = new Integer[triangle.length][triangle.length];
-        //init setting
-        dp[0][0] = value[0][0];
-        for(int i = 0; i<value.length; i++){
-            dfs(value.length - 1, i);
+
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                if (j == 0) {
+                    triangle[i][j] += triangle[i - 1][j];
+                    continue;
+                }
+                if (j == i) {
+                    triangle[i][j] += triangle[i - 1][j - 1];
+                    continue;
+                }
+
+                int left = triangle[i][j] + triangle[i - 1][j - 1];
+                int right = triangle[i][j] + triangle[i - 1][j];
+
+                triangle[i][j] = Math.max(left, right);
+
+                answer = Math.max(answer, triangle[i][j]);
+            }
         }
 
-        int max = -1;
-        for(int i = 0;i<value.length; i++){
-            max = Math.max(dp[value.length-1][i], max);
-        }
-        return max;
+        return answer;
     }
 }
