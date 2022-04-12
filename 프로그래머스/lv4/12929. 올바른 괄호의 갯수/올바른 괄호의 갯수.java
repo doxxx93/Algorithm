81afaa53baacacbd5463b5e3e3369ef341b1b1e5
D <51396905+doxxx93@users.noreply.github.com>
2022-04-12 11:07:20 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 552.72 ms, Memory: 379 MB -BaekjoonHub

--- 프로그래머스/lv4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
+++ 프로그래머스/lv4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
@@ -1,22 +1,43 @@
+import java.util.Stack;
+
 class Solution {
+
     public int solution(int n) {
         int answer = 0;
-        int[][] dp = new int[n+1][n+1];
 
-        dp[1][1] = 1;
+        Stack<P> stack = new Stack<>();
+        stack.push(new P(0, 0)); // 괄호의 개수 0 개부터 시작
+        while (!stack.isEmpty()) {
 
+            P p = stack.pop();
 
-        for(int i = 0; i<=n; i++){
-            dp[0][i] = 1;
-        }
-
-        for(int i = 1; i<=n; i++){
-            for(int j = i; j<=n; j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            if (p.open > n) {
+                continue;
             }
+            if (p.open < p.close) {
+                continue;
+            }
+
+            if (p.open + p.close == 2 * n) {
+                answer++;
+                continue;
+            }
+
+            stack.push(new P(p.open + 1, p.close));
+            stack.push(new P(p.open, p.close + 1));
+
         }
 
+        return answer;
+    }
 
-        return dp[n][n];
+    class P {
+
+        int open, close;
+
+        public P(int open, int close) {
+            this.open = open;
+            this.close = close;
+        }
     }
 }
