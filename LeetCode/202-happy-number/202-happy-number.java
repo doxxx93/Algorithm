a158e83ed5ad787cc99adf8a45c3b51ab0d71a7e
D <51396905+doxxx93@users.noreply.github.com>
2022-04-20 04:16:35 +0900

    Time: 1 ms (93.57%), Space: 41.7 MB (27.29%) - LeetHub

---
+++ LeetCode/202-happy-number/202-happy-number.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public boolean isHappy(int n) {
+
+        int slow = n;
+        int fast = n;
+        do {
+            slow = squareSum(slow);
+            fast = squareSum(squareSum(fast));
+        } while (slow != fast);
+        return slow == 1;
+    }
+
+    // 자리수 더하기
+    private int squareSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
+    }
+}
+        
