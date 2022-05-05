c702204ccde012573a4a92e5d08943a1d7571285
D <51396905+doxxx93@users.noreply.github.com>
2022-05-05 19:15:47 +0900

    Time: 5 ms (88.60%), Space: 45.1 MB (65.65%) - LeetHub

--- LeetCode/12-integer-to-roman/12-integer-to-roman.java
+++ LeetCode/12-integer-to-roman/12-integer-to-roman.java
@@ -1,22 +1,14 @@
 class Solution {
+    final static int[] val = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    final static String[] rom = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
-    private static final int[] values = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4,
-        1};
-    private static final String[] romanNumerals = new String[]{"M", "CM", "D", "CD", "C", "XC", "L",
-        "XL", "X", "IX", "V", "IV", "I"};
-
-    public String intToRoman(int num) {
-
-        int point = 0;
-        StringBuilder numeralResult = new StringBuilder();
-        while (num > 0) {
-            if (num >= values[point]) {
-                numeralResult.append(romanNumerals[point]);
-                num -= values[point];
-            } else {
-                point++;
+    public String intToRoman(int N) {
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; N > 0; i++)
+            while (N >= val[i]) {
+                ans.append(rom[i]);
+                N -= val[i];
             }
-        }
-        return numeralResult.toString();
+        return ans.toString();
     }
 }
