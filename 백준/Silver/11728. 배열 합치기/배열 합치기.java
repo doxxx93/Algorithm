a1a7957995827193b457d3c15c05915600281b26
D <51396905+doxxx93@users.noreply.github.com>
2022-05-23 00:46:38 +0900

    [Silver V] Title: 배열 합치기, Time: 1604 ms, Memory: 307960 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,18 +14,39 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N + M];
-
-        st = new StringTokenizer(br.readLine() + " " + br.readLine());
-        for (int i = 0; i < N + M; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
+        int[] B = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        int index1 = 0;
+        int index2 = 0;
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Arrays.sort(arr);
-        for (int i : arr) {
-            bw.write(i + " ");
+
+        while (index1 < N && index2 < M) {
+            if (A[index1] < B[index2]) {
+                bw.write(A[index1] + " ");
+                index1++;
+            } else {
+                bw.write(B[index2] + " ");
+                index2++;
+            }
         }
+        while (index1 < N) {
+            bw.write(A[index1] + " ");
+            index1++;
+        }
+        while (index2 < M) {
+            bw.write(B[index2] + " ");
+            index2++;
+        }
+
         bw.flush();
         bw.close();
     }
