a4314ae87c62335f2cd8553282cea9e7f0e70309
D <doxxx93@gmail.com>
2022-06-20 14:58:42 +0900

    Time: 21 ms (72.62%), Space: 42.8 MB (88.85%) - LeetHub

--- LeetCode/18-4sum/18-4sum.java
+++ LeetCode/18-4sum/18-4sum.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution {
+
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> answer = new ArrayList<>();
         Arrays.sort(nums);
@@ -12,27 +13,36 @@ class Solution {
                 continue;
             }
             for (int j = i + 1; j < nums.length; j++) {
-                if(j>i+1&& nums[j] == nums[j-1]){
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
+
+                var twoSum = (long) target - nums[i] - nums[j];
                 int left = j + 1;
                 int right = nums.length - 1;
+
                 while (left < right) {
-                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
-                    if (sum == target) {
-                        answer.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        while (left+1<right && nums[left]==nums[left+1]){
-                            left++;
+                    var sumLR = nums[left] + nums[right];
+                    if (sumLR < twoSum) {
+                        left++;
+                    } else if (sumLR > twoSum) {
+                        right--;
+                    } else {
+                        if (left > j + 1 && right < nums.length - 1) {
+                            if (nums[left] != nums[left - 1] || nums[right] != nums[right + 1]) {
+                                answer.add(
+                                    Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                            }
+                        } else {
+                            answer.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
                         }
                         left++;
-                    } else if (sum < target) {
-                        left++;
-                    } else {
                         right--;
                     }
                 }
             }
         }
+
         return answer;
     }
 }
