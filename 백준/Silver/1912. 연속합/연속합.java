47317e4f8fdf08bb0537b41e233259df0ec9186a
D <doxxx93@gmail.com>
2022-06-20 14:18:11 +0900

    [Silver II] Title: 연속합, Time: 264 ms, Memory: 24128 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -3,32 +3,22 @@ import java.util.*;
 
 public class Main {
 
-    static int[] arr;
-    static Integer[] dp;
-    static int max;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        arr = new int[n];
-        dp = new Integer[n];
+        int[] arr = new int[n];
+        int[] dp = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         dp[0] = arr[0];
-        max = arr[0];
+        int max = arr[0];
 
-        recur(n - 1);
-        System.out.println(max);
-    }
-
-    static int recur(int n) {
-        if (dp[n] == null) {
-            dp[n] = Math.max(recur(n - 1) + arr[n], arr[n]);
-
-            max = Math.max(dp[n], max);
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            max = Math.max(max, dp[i]);
         }
-        return dp[n];
+        System.out.println(max);
     }
 }
