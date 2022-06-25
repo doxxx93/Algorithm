ef986eaa4de1e13662f3a6cea628436b4c56bb26
D <doxxx93@gmail.com>
2022-06-26 02:05:30 +0900

    [Silver III] Title: 숫자 야구, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -3,107 +3,68 @@ import java.io.*;
 
 class Main {
 
-    static int n;
-    static List<BaseBallData> inputData = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        n = Integer.parseInt(s);
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] data = new int[n][3];
+        int[] strike = new int[n];
+        int[] ball = new int[n];
 
         for (int i = 0; i < n; i++) {
-            s = br.readLine();
-            StringTokenizer st = new StringTokenizer(s, " ");
-            int data = Integer.parseInt(st.nextToken());
-            int strike = Integer.parseInt(st.nextToken());
-            int ball = Integer.parseInt(st.nextToken());
-
-            BaseBallData baseBallData = new BaseBallData(data, strike, ball);
-            inputData.add(baseBallData);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+            data[i][0] = Character.getNumericValue(s.charAt(0));
+            data[i][1] = Character.getNumericValue(s.charAt(1));
+            data[i][2] = Character.getNumericValue(s.charAt(2));
+            strike[i] = Integer.parseInt(st.nextToken());
+            ball[i] = Integer.parseInt(st.nextToken());
         }
-
-        System.out.println(calBaseBall());
-    }
-
-    static int calBaseBall() {
-        int result = 0;
-
-        for (int i = 123; i <= 987; i++) {
-            if (!checkSameNum(i)) {
-                continue; // 중복숫자
-            }
-
-            int allTestPass = 0;
-
-            for (int j = 0; j < n; j++) {
-                int strikeCount = 0;
-                int ballCount = 0;
-
-                BaseBallData current = inputData.get(j);
-                String currentDataString = Integer.toString(current.data);
-                String myDataString = Integer.toString(i);
-
-                for (int k = 0; k < 3; k++) { // strike
-                    if (currentDataString.charAt(k) == myDataString.charAt(k)) {
-                        strikeCount++;
-                    }
+        int answer = 0;
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                if (i == j) {
+                    continue;
                 }
+                for (int k = 1; k <= 9; k++) {
+                    if (j == k || i == k) {
+                        continue;
+                    }
+                    boolean flag = true;
+                    for (int index = 0; index < n; index++) {
+                        int[] datum = data[index];
+                        int strikeCount = 0;
+                        int ballCount = 0;
 
-                for (int h = 0; h < 3; h++) {
-                    for (int u = 0; u < 3; u++) {
-                        if (myDataString.charAt(h) == currentDataString.charAt(u)) { // ball
-                            if (h != u) {
-                                ballCount++;
-                            }
+                        if (datum[0] == i) {
+                            strikeCount++;
+                        }
+                        if (datum[1] == j) {
+                            strikeCount++;
+                        }
+                        if (datum[2] == k) {
+                            strikeCount++;
+                        }
+                        if (datum[0] == j || datum[0] == k) {
+                            ballCount++;
+                        }
+                        if (datum[1] == i || datum[1] == k) {
+                            ballCount++;
+                        }
+                        if (datum[2] == i || datum[2] == j) {
+                            ballCount++;
+                        }
+                        if (strike[index] != strikeCount || ball[index] != ballCount) {
+                            flag = false;
+                            break;
                         }
                     }
-                }
-
-                if (current.strike == strikeCount && current.ball == ballCount) {
-                    allTestPass++;
+                    if (flag) {
+                        answer++;
+                    }
                 }
             }
-            if (allTestPass == n) {
-                result++;
-            }
         }
-        return result;
-    }
-
-    static boolean checkSameNum(int num) {
-
-        String numString = Integer.toString(num);
-
-        if (numString.charAt(0) == numString.charAt(1)) {
-            return false;
-        }
-
-        if (numString.charAt(1) == numString.charAt(2)) {
-            return false;
-        }
-
-        if (numString.charAt(0) == numString.charAt(2)) {
-            return false;
-        }
-
-        if (numString.charAt(0) == '0' || numString.charAt(1) == '0'
-            || numString.charAt(2) == '0') {
-            return false;
-        }
-
-        return true;
-    }
-
-    static class BaseBallData {
-
-        int data;
-        int strike;
-        int ball;
-
-        public BaseBallData(int data, int strike, int ball) {
-            this.data = data;
-            this.strike = strike;
-            this.ball = ball;
-        }
+        System.out.println(answer);
     }
 }
