015da992ceb127bbc1cf3d17229ce608b4545d2a
D <doxxx93@gmail.com>
2022-07-06 16:18:02 +0900

    [Gold V] Title: 회문은 회문아니야!!, Time: 212 ms, Memory: 19508 KB -BaekjoonHub

--- 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.java
+++ 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.java
@@ -5,18 +5,18 @@ class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        char[] str = br.readLine().toCharArray();
         boolean flag = false;
-        for (int i = 0; i < str.length() / 2; i++) {
-            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
-                System.out.println(str.length());
+        for (int i = 0; i < str.length / 2; i++) {
+            if (str[i] != str[str.length - i - 1]) {
+                System.out.println(str.length);
                 return;
-            } else if (str.charAt(i) != str.charAt(i + 1)) {
+            } else if (str[i] != str[i + 1]) {
                 flag = true;
             }
         }
         if (flag) {
-            System.out.println(str.length() - 1);
+            System.out.println(str.length - 1);
         } else {
             System.out.println(-1);
         }
