aced24bf7ec04cd3930ff21cf6847676a1fa0ddc
D <doxxx93@gmail.com>
2022-07-12 23:56:14 +0900

    [Silver IV] Title: 청기 백기, Time: 120 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int W = 0;
+        int B = 0;
+        int tmp = 2;
+        while (true) {
+            W++;
+            B = B + tmp;
+            tmp += 2;
+            if (W + B >= num)
+                break;
+        }
+        System.out.println(W);
+    }
+}
