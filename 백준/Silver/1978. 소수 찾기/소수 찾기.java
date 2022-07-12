0148bd992115fc64fd3c15c1e5f03af070ecf123
D <doxxx93@gmail.com>
2022-07-12 22:39:49 +0900

    [Silver V] Title: 소수 찾기, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.java
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -1,30 +1,30 @@
+import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-        
-        int count = 0;
 
-        for(int i=0;i<N;i++){
-            arr[i] = sc.nextInt();
-            
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            count += isPrime(Integer.parseInt(st.nextToken()));
         }
-        for(int i = 0;i<N;i++){
-            count += isPrime(arr[i]);
-        }
+
         System.out.println(count);
     }
-    
+
     public static int isPrime(int n) {
-        if(n==1) return 0;
-        for (int i = 2; i<=(int)Math.sqrt(n); i++) {
+        if (n == 1) {
+            return 0;
+        }
+        for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) {
                 return 0;
             }
         }
-	    return 1;
-}
+        return 1;
+    }
 }
