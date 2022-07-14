e99e6a0b2535f5ee5fbca6995da127dddf3c0593
D <doxxx93@gmail.com>
2022-07-14 23:55:47 +0900

    [Gold V] Title: 공약수, Time: 156 ms, Memory: 16112 KB -BaekjoonHub

--- 백준/Gold/2436. 공약수/공약수.java
+++ 백준/Gold/2436. 공약수/공약수.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 
 public class Main {
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,17 +15,15 @@ public class Main {
         long resA = 0, rebB = 0;
         for (int i = 1; i * i <= tmp; i++) {
             if (tmp % i == 0) {
-                int a = i;
                 int b = tmp / i;
-                if (getGCD(a, b) == 1) {
-                    resA = a * gcd;
+                if (getGCD(i, b) == 1) {
+                    resA = i * gcd;
                     rebB = b * gcd;
                 }
             }
         }
         System.out.println(resA + " " + rebB);
     }
-
 
     static int getGCD(int a, int b) {
         return a % b == 0 ? b : getGCD(b, a % b);
