61767b42b5e488f2c631a1f8f48dc3f8ef08a4c3
D <doxxx93@gmail.com>
2022-07-20 04:51:53 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public final class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0;
+        int maxIndex = 1;
+        for (int i = 1; i <= 9; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n > max) {
+                max = n;
+                maxIndex = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(maxIndex);
+    }
+}
