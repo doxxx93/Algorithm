2c6bf4544733bdf41e205dcf69dd38adf847526f
D <doxxx93@gmail.com>
2022-07-28 05:31:05 +0900

    [Silver V] Title: 숫자 카드, Time: 1328 ms, Memory: 109944 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -8,14 +8,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
         Arrays.sort(arr);
 
         StringBuilder sb = new StringBuilder();
 
         int M = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < M; i++) {
             int target = Integer.parseInt(st.nextToken());
             if (Arrays.binarySearch(arr, target) < 0) {
