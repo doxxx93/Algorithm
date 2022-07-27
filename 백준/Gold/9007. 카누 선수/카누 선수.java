614d0d4d9e71257c5fd5ee76d8e8211afd282802
D <doxxx93@gmail.com>
2022-07-27 12:29:48 +0900

    [Gold III] Title: 카누 선수, Time: 3244 ms, Memory: 122888 KB -BaekjoonHub

--- 백준/Gold/9007. 카누 선수/카누 선수.java
+++ 백준/Gold/9007. 카누 선수/카누 선수.java
@@ -24,50 +24,42 @@ public class Main {
                 }
             }
 
-            merge = new int[2][n * n];
+            int[] sum1 = new int[n * n];
+            int[] sum2 = new int[n * n];
+
+            int cnt = 0;
+
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    merge[0][i * n + j] = arr[0][i] + arr[1][j];
-                    merge[1][i * n + j] = arr[2][i] + arr[3][j];
+                    sum1[cnt] = arr[0][i] + arr[1][j];
+                    sum2[cnt++] = arr[2][i] + arr[3][j];
                 }
             }
-            for (int i = 0; i < 2; i++) {
-                Arrays.sort(merge[i]);
+
+            Arrays.sort(sum1);
+            Arrays.sort(sum2);
+
+            int s = 0;
+            int e = sum2.length - 1;
+            int ans = sum1[s] + sum2[e];
+
+            while ((s < sum1.length) && (e >= 0)) {
+                if (Math.abs(ans - k) == Math.abs(sum1[s] + sum2[e] - k)) {
+                    ans = Math.min(ans, sum1[s] + sum2[e]);
+                } else if (Math.abs(ans - k) > Math.abs(sum1[s] + sum2[e] - k)) {
+                    ans = sum1[s] + sum2[e];
+                }
+
+                if (sum1[s] + sum2[e] >= k) {
+                    e -= 1;
+                } else {
+                    s += 1;
+                }
             }
-            sb.append(binarySearch(n, k)).append("\n");
+
+            sb.append(ans + "\n");
         }
 
         System.out.println(sb);
-    }
-
-    private static int binarySearch(int n, int k) {
-        int v = Integer.MAX_VALUE;
-        int answer = -1;
-
-        for (int i = 0; i < n * n; i++) {
-            int a = merge[0][i];
-            int s = 0;
-            int e = n * n - 1;
-
-            while (s < e) {
-                int m = (s + e) / 2;
-                if (a + merge[1][m] < k) {
-                    s = m + 1;
-                } else {
-                    e = m;
-                }
-            }
-            if (s > 0) {
-                if (v >= Math.abs(a + merge[1][s - 1] - k)) {
-                    v = Math.abs(a + merge[1][s - 1] - k);
-                    answer = a + merge[1][s - 1];
-                }
-            }
-            if (v > Math.abs(a + merge[1][s] - k)) {
-                v = Math.abs(a + merge[1][s] - k);
-                answer = a + merge[1][s];
-            }
-        }
-        return answer;
     }
 }
