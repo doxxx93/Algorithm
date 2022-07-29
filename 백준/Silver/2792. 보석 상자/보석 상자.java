d44323c432f7c9ba3652a4259568bc756bda41fc
D <doxxx93@gmail.com>
2022-07-30 03:58:52 +0900

    [Silver II] Title: 보석 상자, Time: 384 ms, Memory: 34776 KB -BaekjoonHub

---
+++ 백준/Silver/2792. 보석 상자/보석 상자.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[m];
+        int l = 1;
+        int r = Integer.MIN_VALUE;
+        for (int i = 0; i < m; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            r = Math.max(r, arr[i]);
+        }
+
+        int max = 0;
+        int answer = 0;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            int sum = 0;
+
+            for (int i = 0; i < m; i++) {
+                if (arr[i] % mid == 0) {
+                    sum += arr[i] / mid;
+                } else {
+                    sum += arr[i] / mid + 1;
+                }
+            }
+
+            if (sum > n) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+                answer = mid;
+            }
+        }
+        System.out.println(answer);
+    }
+}
