585cd9e9b7b8bb99351b76881b749a4461e2ce1e
D <doxxx93@gmail.com>
2022-08-07 04:32:40 +0900

    [Silver I] Title: 작은 벌점, Time: 1956 ms, Memory: 17284 KB -BaekjoonHub

--- 백준/Silver/16498. 작은 벌점/작은 벌점.java
+++ 백준/Silver/16498. 작은 벌점/작은 벌점.java
@@ -1,46 +1,32 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int a, b, c;
-    static int[] card_A;
-    static int[] card_B;
-    static int[] card_C;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int result = Integer.MAX_VALUE;
-        String[] st = br.readLine().split(" ");
-        a = Integer.parseInt(st[0]);
-        b = Integer.parseInt(st[1]);
-        c = Integer.parseInt(st[2]);
-
-        card_A = new int[a];
-        card_B = new int[b];
-        card_C = new int[c];
-
-        for(int i=0;i<3;i++) {
-            String[] st2 = br.readLine().split(" ");
-            for(int j=0;j<st2.length;j++) {
-                if(i==0)
-                    card_A[j] = Integer.parseInt(st2[j]);
-                if(i==1)
-                    card_B[j] = Integer.parseInt(st2[j]);
-                if(i==2)
-                    card_C[j] = Integer.parseInt(st2[j]);
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[][] arr = new int[3][];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = new int[Integer.parseInt(st.nextToken())];
         }
 
-        for(int i=0;i< a;i++) {
-            for(int j=0;j< b;j++) {
-                for(int k=0;k< c;k++) {
-                    result = Math.min(result,Math.abs(Math.max(Math.max(card_A[i], card_B[j]),card_C[k])- Math.min(Math.min(card_A[i], card_B[j]),card_C[k])));
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < arr[0].length; i++) {
+            for (int j = 0; j < arr[1].length; j++) {
+                for (int k = 0; k < arr[2].length; k++) {
+                    answer = Math.min(answer, Math.abs(
+                        Math.max(Math.max(arr[0][i], arr[1][j]), arr[2][k]) - Math.min(
+                            Math.min(arr[0][i], arr[1][j]), arr[2][k])));
                 }
             }
         }
-
-        bw.write(result + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(answer);
     }
 }
