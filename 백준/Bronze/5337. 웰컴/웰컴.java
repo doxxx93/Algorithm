af1a160783d02081df2bce80af2d9691168c84f8
D <doxxx93@gmail.com>
2022-09-11 15:09:14 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(".  .   .\n"
+                           + "|  | _ | _. _ ._ _  _\n"
+                           + "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
