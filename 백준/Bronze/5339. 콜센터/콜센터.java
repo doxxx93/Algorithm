3721e38b2840eb3dc5eb5ffb91a3d00f2512ce14
D <doxxx93@gmail.com>
2022-09-21 23:05:27 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,15 @@
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("     /~\\\n" +
+                           "    ( oo|\n" +
+                           "    _\\=/_\n" +
+                           "   /  _  \\\n" +
+                           "  //|/.\\|\\\\\n" +
+                           " ||  \\ /  ||\n" +
+                           "============\n" +
+                           "|          |\n" +
+                           "|          |\n" +
+                           "|          |\n");
+    }
+}
