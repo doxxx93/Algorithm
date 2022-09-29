a8608f16f95356f82ecea435161be6bfb59b3683
D <doxxx93@gmail.com>
2022-09-30 06:52:13 +0900

    [Bronze V] Title: 나는 행복합니다~, Time: 148 ms, Memory: 16028 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int r = k / m;
+        int c = k % m;
+        System.out.println(r + " " + c);
+    }
+}
