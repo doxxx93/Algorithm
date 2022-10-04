af53da50f39e00f21e3887ecbf184d368f856088
D <doxxx93@gmail.com>
2022-10-04 11:19:45 +0900

    [Gold V] Title: 숫자고르기, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static int[] arr;
+    static boolean[] visited;
+    static Set<Integer> result;
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        visited = new boolean[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        result = new TreeSet<>();
+        count = 0;
+        for (int i = 1; i < n + 1; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            dfs(i);
+            visited = new boolean[n + 1];
+        }
+        System.out.println(count);
+        Collections.sort(new ArrayList<>(result));
+        for (int i : result) {
+            System.out.println(i);
+        }
+    }
+
+    public static void dfs(int cur) {
+        visited[cur] = true;
+        int next = arr[cur];
+        if (visited[next]) {
+            if (result.contains(next)) {
+                return;
+            }
+            result.add(next);
+            count++;
+            return;
+        }
+        dfs(next);
+    }
+}
