1b05f68357968a3be7fcd71b7ee47aaf2cee3a43
D <doxxx93@gmail.com>
2022-11-01 06:20:33 +0900

    [Bronze IV] Title: 세수정렬, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.java
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -5,12 +5,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Set<Integer> set = new TreeSet<>();
+        List<Integer> list = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 3; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
+            list.add(Integer.parseInt(st.nextToken()));
         }
-        for (Integer integer : set) {
+        Collections.sort(list);
+        for (Integer integer : list) {
             System.out.print(integer + " ");
         }
     }
