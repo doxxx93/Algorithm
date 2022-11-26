2f3a5f13de45b7e1cf0b4f0dcf47d375902e8148
D <doxxx93@gmail.com>
2022-11-26 21:23:49 +0900

    [Bronze III] Title: 공, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int num = 1;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if (x == num) {
+                num = y;
+                continue;
+            }
+            if (y == num) {
+                num = x;
+            }
+        }
+
+        System.out.println(num);
+    }
+}
