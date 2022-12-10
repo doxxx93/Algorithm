4c17934a7fa39b935b78538ad94bccdec19ac587
D <doxxx93@gmail.com>
2022-12-11 06:07:34 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 112 ms, Memory: 13828 KB -BaekjoonHub

--- 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -1,14 +1,13 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.printf("%-15s%-15s%-11s%-10s\n", "SHIP NAME", "CLASS", "DEPLOYMENT", "IN SERVICE");
-        System.out.printf("%-15s%-15s%-11s%-10s\n", "N2 Bomber", "Heavy Fighter", "Limited", "21");
-        System.out.printf("%-15s%-15s%-11s%-10s\n", "J-Type 327", "Light Combat", "Unlimited", "1");
-        System.out.printf("%-15s%-15s%-11s%-10s\n", "NX Cruiser", "Medium Fighter", "Limited", "18");
-        System.out.printf("%-15s%-15s%-11s%-10s\n", "N1 Starfighter", "Medium Fighter", "Unlimited", "25");
-        System.out.printf("%-15s%-15s%-11s%-10s\n", "Royal Cruiser", "Light Combat", "Limited", "4");
+        System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n"
+                           + "N2 Bomber      Heavy Fighter  Limited    21        \n"
+                           + "J-Type 327     Light Combat   Unlimited  1         \n"
+                           + "NX Cruiser     Medium Fighter Limited    18        \n"
+                           + "N1 Starfighter Medium Fighter Unlimited  25        \n"
+                           + "Royal Cruiser  Light Combat   Limited    4         ");
     }
 }
