85f2f54d31056672debb1eed65390b402cf9d37e
D <doxxx93@gmail.com>
2022-12-12 16:08:11 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 124 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int s = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        System.out.println((d / 2 / s) * t);
+    }
+}
