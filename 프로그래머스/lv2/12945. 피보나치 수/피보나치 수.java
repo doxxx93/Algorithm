7a9d6f1c3af0c94d022a69c35e0f4bcd666d9ee7
D <doxxx93@gmail.com>
2022-12-15 18:43:40 +0900

    [level 2] Title: 피보나치 수, Time: 13.25 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -3,22 +3,19 @@ class Solution {
     int[] check = new int[100001];
 
     public int solution(int n) {
-        int answer = dp(n);
-        return answer;
+        return dp(n);
     }
 
     public int dp(int n) {
-        if (n == 1) {
-            return 1;
+        if (n == 0) {
+            return 0;
         }
-        if (n == 2) {
+        if (n == 1 || n == 2) {
             return 1;
         }
         if (check[n] != 0) {
             return check[n];
         }
-        check[n] = dp(n - 1) % 1234567 + dp(n - 2) % 1234567;
-        return check[n] % 1234567;
+        return (check[n] = dp(n - 1) % 1234567 + dp(n - 2) % 1234567 )% 1234567;
     }
-
 }
