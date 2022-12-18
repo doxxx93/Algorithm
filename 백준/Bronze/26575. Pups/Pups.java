30eed181e3f534b0c3633d2d04561ecab12a0113
D <doxxx93@gmail.com>
2022-12-18 20:06:36 +0900

    [Bronze V] Title: Pups, Time: 136 ms, Memory: 14532 KB -BaekjoonHub

---
+++ 백준/Bronze/26575. Pups/Pups.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            double d = Double.parseDouble(st.nextToken());
+            double f = Double.parseDouble(st.nextToken());
+            double p = Double.parseDouble(st.nextToken());
+            System.out.printf("$%.2f", (d * f * p));
+            System.out.println();
+        }
+    }
+}
