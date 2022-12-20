34bb80627148b77f1541c3c7a3365fe561bde9f9
D <doxxx93@gmail.com>
2022-12-20 20:17:38 +0900

    [level 1] Title: 모의고사, Time: 3.44 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,36 +1,26 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-    public int[] solution(int[] answers) {
-        int[] answer = {};
-        int[] pick1 = {1, 2, 3, 4, 5};
-        int[] pick2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] pick3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        int[] count = new int[3];
-        for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == pick1[i % 5]) count[0]++;
-            if(answers[i] == pick2[i % 8]) count[1]++;
-            if(answers[i] == pick3[i % 10]) count[2]++;
-            
-        }
+    public int[] solution(int[] answers) {
+        List<Integer> answer = new ArrayList<>();
+        int[][] picks = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
         int max = 0;
-        for(int i = 0; i < 3; i++) {
-            if(count[i] > max) {
-                max = count[i];
+        int[] count = new int[picks.length];
+        for (int i = 0; i < answers.length; i++) {
+            for (int j = 0; j < picks.length; j++) {
+                if (answers[i] == picks[j][i % picks[j].length]) {
+                    count[j]++;
+                    max = Math.max(max, count[j]);
+                }
             }
-            
         }
-        for(int i = 0; i < 3; i++) {
-            if(count[i] == max) {
-                answer = Arrays.copyOf(answer, answer.length + 1);
-                answer[answer.length - 1] = i + 1;
-                
-            }
-            
-        }
-        return answer;
-            
         
+        for (int i = 0; i < 3; i++) {
+            if (count[i] == max) {
+                answer.add(i + 1);
+            }
+        }
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
