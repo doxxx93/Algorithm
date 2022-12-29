63fcd5a145f1b75d06a6b93812557c643a452c06
D <doxxx93@gmail.com>
2022-12-29 21:14:09 +0900

    [level 2] Title: 모음 사전, Time: 0.04 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/README.md
+++ 프로그래머스/lv2/84512. 모음 사전/README.md
@@ -1,14 +1,14 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/84512) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 75.1 MB, 시간: 0.03 ms
+메모리: 76.2 MB, 시간: 0.04 ms
 
 ### 구분
 
-코딩테스트 연습 > 위클리 챌린지
+코딩테스트 연습 > 완전탐색
 
 ### 채점결과
 
--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -1,30 +1,16 @@
 class Solution {
+
     public int solution(String word) {
         int answer = 0;
-
-        char[] vowel = {'A', 'B', 'C', 'D', 'E'};
         int max = 3905;
-
+        char[] vowels = {'A', 'E', 'I', 'O', 'U'};
         for (int i = 1; i <= word.length(); i++) {
-            switch (word.charAt(i-1)) {
-                case 'A'-> {
-                    answer += 1;
-                }
-                case 'E'->{
-                    answer += ((max / Math.pow(5, i)) * 1) + 1;
-                }
-                case 'I'->{
-                    answer += ((max / Math.pow(5, i)) * 2) + 1;
-                }
-                case 'O' ->{
-                    answer += ((max / Math.pow(5, i)) * 3) + 1;
-                }
-                case 'U' ->{
-                    answer += ((max / Math.pow(5, i)) * 4) + 1;
+            for (int j = 0; j < vowels.length; j++) {
+                if (word.charAt(i - 1) == vowels[j]) {
+                    answer += ((max / Math.pow(5, i)) * j) + 1;
                 }
             }
         }
-
         return answer;
     }
 }
