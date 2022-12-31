41a840a58a15f85b2cbda7d67b0ffce49a9c4a19
D <doxxx93@gmail.com>
2022-12-31 18:01:24 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 144 ms, Memory: 16140 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -3,56 +3,48 @@ import java.util.*;
 
 public class Main {
 
-    static int max;
-    static int min;
-    static int[] operator = new int[4];
-    static int[] number;
-    static int n;
+    static int n,min, max;
+    static int[] numbers;
+    static int[] operators;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        number = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            number[i] = Integer.parseInt(st.nextToken());
-        }
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 4; i++) {
-            operator[i] = Integer.parseInt(st.nextToken());
-        }
-
+        numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt)
+            .toArray();
+        operators = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt)
+            .toArray();
         max = Integer.MIN_VALUE;
         min = Integer.MAX_VALUE;
-
-        dfs(1, number[0]);
-
+        dfs(1, numbers[0]);
         System.out.println(max);
         System.out.println(min);
     }
 
-    private static void dfs(int index, int result) {
-        if (index == n) {
-            max = Math.max(max, result);
-            min = Math.min(min, result);
+    private static void dfs(int i, int number) {
+        if (i == n) {
+            max = Math.max(max, number);
+            min = Math.min(min, number);
             return;
         }
-
-        for (int i = 0; i < 4; i++) {
-            if (operator[i] > 0) {
-                operator[i]--;
-                if (i == 0) {
-                    dfs(index + 1, result + number[index]);
-                } else if (i == 1) {
-                    dfs(index + 1, result - number[index]);
-                } else if (i == 2) {
-                    dfs(index + 1, result * number[index]);
-                } else if (i == 3) {
-                    dfs(index + 1, result / number[index]);
+        for (int j = 0; j < 4; j++) {
+            if (operators[j] > 0) {
+                operators[j]--;
+                switch (j) {
+                    case 0:
+                        dfs(i + 1, number + numbers[i]);
+                        break;
+                    case 1:
+                        dfs(i + 1, number - numbers[i]);
+                        break;
+                    case 2:
+                        dfs(i + 1, number * numbers[i]);
+                        break;
+                    case 3:
+                        dfs(i + 1, number / numbers[i]);
+                        break;
                 }
-                operator[i]++;
+                operators[j]++;
             }
         }
     }
