6379f5d234cd6c2050fdc7def8f8fd043eb194d6
D <doxxx93@gmail.com>
2022-12-31 16:23:20 +0900

    [level 2] Title: 프린터, Time: 0.45 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/README.md
+++ 프로그래머스/lv2/42587. 프린터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프린터 - 42587 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/42587) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 0.71 ms
+메모리: 74.5 MB, 시간: 0.45 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42587. 프린터/프린터.java
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -1,17 +1,13 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
-
     public int solution(int[] priorities, int location) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         int answer = 0;
-
-        for (int i = 0; i < priorities.length; i++) {
-            pq.add(priorities[i]);
+        Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int priority : priorities) {
+            pq.add(priority);
         }
-
-        while (!pq.isEmpty()) {
+        while(!pq.isEmpty()){
             for (int i = 0; i < priorities.length; i++) {
                 if (priorities[i] == pq.peek()) {
                     if (i == location) {
