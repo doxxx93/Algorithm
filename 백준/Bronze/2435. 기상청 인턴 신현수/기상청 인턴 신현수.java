504fe20a0f86c7debb67a127ed8a14c635913697
D <doxxx93@gmail.com>
2023-01-03 16:59:28 +0900

    [Bronze I] Title: 기상청 인턴 신현수, Time: 136 ms, Memory: 14456 KB -BaekjoonHub

---
+++ 백준/Bronze/2435. 기상청 인턴 신현수/기상청 인턴 신현수.java
@@ -0,0 +1,23 @@
+import java.io. *;
+import java.util. *;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        List<Integer> sumList = new ArrayList<>();
+        for (int i = 0; i < n - k + 1; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k; j++) {
+                sum += arr[j];
+            }
+            sumList.add(sum);
+        }
+        Collections.sort(sumList);
+        System.out.println(sumList.get(sumList.size() - 1));
+    }
+}
