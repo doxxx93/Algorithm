82baa1888ef20e0502ce3e3e977a28c733885ba3
D <doxxx93@gmail.com>
2023-01-12 18:44:22 +0900

    [Bronze III] Title: 통학의 신, Time: 128 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.java
+++ 백준/Bronze/17945. 통학의 신/통학의 신.java
@@ -5,23 +5,15 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // -1000 ≤ A, B ≤ 1000
         StringTokenizer st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        Set<Integer> x = new TreeSet<>();
-        // x 는 항상 정수이다.
-        // x * x + 2 * a * x + b = 0
-        // (x - p)(x-q) = 0
-        // p * q= b, -(p+q) = + 2 * a
+        StringBuilder sb = new StringBuilder();
         for (int i = -(int) Math.sqrt(1000); i * i <= 1000; i++) {
             if (i * i + 2 * a * i + b == 0) {
-                x.add(i);
+                sb.append(i).append(" ");
             }
         }
-        for (Integer answer : x) {
-            System.out.print(answer+ " ");
-        }
-        System.out.println();
+        System.out.println(sb);
     }
 }
