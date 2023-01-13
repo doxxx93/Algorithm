7bd5121b55b1672b11b18cc5d8d6a7d5754caf85
D <doxxx93@gmail.com>
2023-01-13 21:10:43 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
@@ -1,16 +1,15 @@
 import java.io.*;
-import java.util.*;
 
-class Main {
+public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int count = 0;
-        for (int t = 2; t < n; t += 2) {
-            for (int y = 1; y < n; y++) {
-                for (int i = y + 2; i < n; i++) {
-                    if (i + y + t == n) {
+        for (int a = 1; a < 100; a++) {
+            for (int b = 1; b < 100; b++) {
+                if (a % 2 == 0) {
+                    if (n >= a + 2 * b + 2) {
                         count++;
                     }
                 }
