eacf64f2c3c2d1ca9707fd26a8059c41227128f0
D <doxxx93@gmail.com>
2023-01-15 22:28:45 +0900

    [Bronze II] Title: 방 배정하기, Time: 116 ms, Memory: 13992 KB -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.java
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.java
@@ -11,29 +11,20 @@ class Main {
         int c = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
 
-        if (a == 1) {
-            System.out.println(1);
-            return;
-        }
-        if (c % a == 0 && b % a == 0) {
-            if (n % a == 0) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
-            }
-            return;
-        }
-        for (int i = 0; i <= 50; i++) {
-            for (int j = 0; j <= 50; j++) {
-                for (int k = 0; k <= 50; k++) {
-                    int sum = i * c + j * b + k * a;
-                    if (sum == n) {
-                        System.out.println(1);
-                        return;
+        int count = 0;
+        for (int i = 0; i < n / c; i++) {
+            for (int j = 0; j < n / b; j++) {
+                for (int k = 0; k < n / a; k++) {
+                    if (i * c + j * b + k * a == n) {
+                        count++;
                     }
                 }
             }
         }
-        System.out.println(0);
+        if (count == 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(1);
+        }
     }
 }
