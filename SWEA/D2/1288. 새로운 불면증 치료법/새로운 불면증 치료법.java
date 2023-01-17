b5bab5be3d97a32004007f9f9918b64258806370
D <doxxx93@gmail.com>
2023-01-17 19:53:12 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 100 ms, Memory: 18,620 KB -BaekjoonHub

---
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+class Solution {
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int test_case = 1; test_case <= T; test_case++) {
+            long n = Long.parseLong(br.readLine());
+            long con = n;
+            int check = 0;
+            while (true) {
+                long temp = n;
+                while (temp > 0) {
+                    check |= 1 << (temp % 10);
+                    temp /= 10;
+                }
+                if (check == 1023) {
+                    break;
+                }
+                n += con;
+            }
+            sb.append("#").append(test_case).append(" ").append(n).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
