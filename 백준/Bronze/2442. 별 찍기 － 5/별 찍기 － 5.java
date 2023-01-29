6572ca1d05b257343384fb3e1050dc77af45a257
D <doxxx93@gmail.com>
2023-01-29 13:50:07 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 312 ms, Memory: 16556 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
