0865e20a136e2f914ddc6032216b56cca50c8cd0
D <doxxx93@gmail.com>
2023-01-31 23:08:03 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 832 ms, Memory: 120148 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -8,13 +8,14 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
         int[][] prefix = new int[n + 1][n + 1];
 
-        for (int x = 1; x < n + 1; x++) {
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int y = 1; y < n + 1; y++) {
-                prefix[x][y] = prefix[x][y - 1] +prefix[x-1][y] -prefix[x-1][y-1] + Integer.parseInt(st.nextToken()) ;
+            for (int j = 1; j <= n; j++) {
+                prefix[i][j] =
+                    prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + Integer.parseInt(
+                        st.nextToken());
             }
         }
         StringBuilder sb = new StringBuilder();
@@ -24,7 +25,9 @@ public class Main {
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            sb.append(prefix[x2][y2] - prefix[x1-1][y2] - prefix[x2][y1-1] + prefix[x1-1][y1-1] + "\n");
+            sb.append(
+                    prefix[x2][y2] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1])
+                .append('\n');
         }
         System.out.println(sb);
     }
