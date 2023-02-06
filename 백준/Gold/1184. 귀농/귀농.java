60033aa57370c8a391b9afdf3b2618fcc9b0386d
doxxx <doxxx93@gmail.com>
2023-02-06 22:59:44 +0900

    [Gold I] Title: 귀농, Time: 568 ms, Memory: 43708 KB -BaekjoonHub

--- 백준/Gold/1184. 귀농/귀농.java
+++ 백준/Gold/1184. 귀농/귀농.java
@@ -1,24 +1,23 @@
-import java.io.*;
-import java.util.*;
+import java.io. *;
+import java.util. *;
 
 public class Main {
 
     static int n;
     static int[][] prefix;
-    static Map<Integer, Integer> bio;
+    static int[] bio;
+    static int MAX = 2500000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         prefix = new int[n + 1][n + 1];
-        bio = new HashMap<>();
+        bio = new int[2 * MAX];
         StringTokenizer st;
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= n; j++) {
-                prefix[i][j] =
-                    prefix[i][j - 1] + prefix[i - 1][j] - prefix[i - 1][j - 1] + Integer.parseInt(
-                        st.nextToken());
+                prefix[i][j] = prefix[i][j - 1] + prefix[i - 1][j] - prefix[i - 1][j - 1] + Integer.parseInt(st.nextToken());
             }
         }
 
@@ -28,32 +27,32 @@ public class Main {
             for (int j = 1; j <= n; j++) {
                 for (int x = 1; x < i; x++) {
                     for (int y = 1; y < j; y++) {
-                        bio.merge(area(x, y, i - 1, j - 1), 1, Integer::sum);
+                        bio[val(x, y, i - 1, j - 1) + MAX]++;
                     }
                 }
                 for (int x = i; x <= n; x++) {
                     for (int y = j; y <= n; y++) {
-                        count += bio.getOrDefault(area(i, j, x, y), 0);
+                        count += bio[val(i, j, x, y) + MAX];
                     }
                 }
                 for (int x = 1; x < i; x++) {
                     for (int y = 1; y < j; y++) {
-                        bio.merge(area(x, y, i - 1, j - 1), -1, Integer::sum);
+                        bio[val(x, y, i - 1, j - 1) + MAX]--;
                     }
                 }
                 for (int x = 1; x < i; x++) {
                     for (int y = j; y <= n; y++) {
-                        bio.merge(area(x, j, i - 1, y), 1, Integer::sum);
+                        bio[val(x, j, i - 1, y) + MAX]++;
                     }
                 }
                 for (int x = i; x <= n; x++) {
                     for (int y = 1; y < j; y++) {
-                        count += bio.getOrDefault(area(i, y, x, j - 1), 0);
+                        count += bio[val(i, y, x, j - 1) + MAX];
                     }
                 }
                 for (int x = 1; x < i; x++) {
                     for (int y = j; y <= n; y++) {
-                        bio.merge(area(x, j, i - 1, y), -1, Integer::sum);
+                        bio[val(x, j, i - 1, y) + MAX]--;
                     }
                 }
             }
@@ -62,7 +61,7 @@ public class Main {
         System.out.println(count);
     }
 
-    private static int area(int x1, int y1, int x2, int y2) {
+    private static int val(int x1, int y1, int x2, int y2) {
         return (prefix[x2][y2] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1]);
     }
 }
