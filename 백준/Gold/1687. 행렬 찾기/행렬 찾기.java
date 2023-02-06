8da3ea550aadc6c69166439d8f13f5ddbd9fabd4
doxxx <doxxx93@gmail.com>
2023-02-07 01:59:35 +0900

    [Gold III] Title: 행렬 찾기, Time: 392 ms, Memory: 27268 KB -BaekjoonHub

--- 백준/Gold/1687. 행렬 찾기/행렬 찾기.java
+++ 백준/Gold/1687. 행렬 찾기/행렬 찾기.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Stream;
 
 public class Main {
 
@@ -10,44 +9,35 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[][] matrixPrefix = new int[n + 1][m + 1];
-
-        for (int row = 1; row < n + 1; row++) {
-            int[] input = Stream.of(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
-            for (int col = 1; col < m + 1; col++) {
-                matrixPrefix[row][col] =
-                    matrixPrefix[row - 1][col] + matrixPrefix[row][col - 1] - matrixPrefix[row - 1][
-                        col - 1] + input[col - 1];
+        int[][] arr = new int[n + 1][m + 1];
+        for (int i = 0; i < n; i++) {
+            final int[] ints = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt)
+                .toArray();
+            if (m >= 0) {
+                System.arraycopy(ints, 0, arr[i + 1], 1, m);
             }
         }
-        int result = 0;
-        for (int row2 = n; row2 >= 1; row2--) {
-            for (int col2 = m; col2 >= 1; col2--) {
-                for (int row1 = row2; row1 >= 1; row1--) {
-                    int col1 = 0;
-                    int s = 1;
-                    int e = col2;
-                    while (s <= e) {
-                        int mid = (s + e) / 2;
-                        if (result < (row2 - row1 + 1) * (col2 - mid + 1)) {
-                            col1 = mid;
-                            s = mid + 1;
-                        } else {
-                            e = mid - 1;
-                        }
-                    }
-                    for (; col1 >= 1; col1--) {
-                        if (matrixPrefix[row2][col2] - matrixPrefix[row2][col1 - 1]
-                            - matrixPrefix[row1 - 1][col2] + matrixPrefix[row1 - 1][col1 - 1]
-                            == 0) {
-                            result = (row2 - row1 + 1) * (col2 - col1 + 1);
-                        } else {
+        int maxArea = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (arr[i][j] == 0) {
+                    arr[i][j] = arr[i - 1][j] + 1;
+                } else {
+                    arr[i][j] = 0;
+                }
+
+                if (arr[i][j] > 0) {
+                    int min = arr[i][j];
+                    for (int k = j; k >= 1; k--) {
+                        if (arr[i][k] == 0) {
                             break;
                         }
+                        min = Math.min(min, arr[i][k]);
+                        maxArea = Math.max(maxArea, min * (j - k + 1));
                     }
                 }
             }
         }
-        System.out.println(result);
+        System.out.println(maxArea);
     }
 }
