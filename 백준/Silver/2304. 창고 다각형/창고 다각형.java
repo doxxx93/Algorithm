557e6473a3e071968f2716cd9ce8d4d394aadc2c
doxxx <doxxx93@gmail.com>
2023-02-06 17:28:31 +0900

    [Silver II] Title: 창고 다각형, Time: 144 ms, Memory: 14596 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,54 +1,32 @@
-import java.io.*;
-import java.util.*;
+import java.io. *;
+import java.util. *;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[1001];
         int n = Integer.parseInt(br.readLine());
-
-        List<Pillar> al = new ArrayList<>();
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            al.add(new Pillar(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            int l = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+            arr[l] = h;
         }
-        Collections.sort(al);
 
+        int[] prefix = new int[1002];
+        int[] suffix = new int[1002];
+        for (int i = 1; i <= 1000; i++) {
+            prefix[i] = Math.max(prefix[i - 1], arr[i]);
+        }
+        for (int i = 1000; i >= 1; i--) {
+            suffix[i] = Math.max(suffix[i + 1], arr[i]);
+        }
         int answer = 0;
-        int max = 0;
-        Pillar current = al.get(0);
-        for (int i = 1; i < n; i++) {
-            if (current.h <= al.get(i).h) {
-                answer += (al.get(i).l - current.l) * current.h;
-                current = al.get(i);
-                max = i;
-            }
+        for (int i = 1; i <= 1000; i++) {
+            answer += Math.min(prefix[i], suffix[i]);
         }
-        current = al.get(al.size() - 1);
-        for (int i = 0; i < n - max; i++) {
-            if (current.h <= al.get(n-1-i).h) {
-                answer += (current.l - al.get(n-1-i).l) * current.h;
-                current = al.get(n-1-i);
-            }
-        }
-        answer += current.h;
         System.out.println(answer);
-    }
-
-    static class Pillar implements Comparable<Pillar> {
-
-        int l;
-        int h;
-
-        public Pillar(int l, int h) {
-            this.l = l;
-            this.h = h;
-        }
-
-        @Override
-        public int compareTo(Pillar o) {
-            return this.l - o.l;
-        }
     }
 }
