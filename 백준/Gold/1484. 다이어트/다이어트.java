94d64838ade00e089ccb0c3ed45a96274f3fe0ff
doxxx <doxxx93@gmail.com>
2023-02-11 17:29:34 +0900

    [Gold V] Title: 다이어트, Time: 132 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Gold/1484. 다이어트/다이어트.java
+++ 백준/Gold/1484. 다이어트/다이어트.java
@@ -1,36 +1,30 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int g = Integer.parseInt(br.readLine());
-
-        // 현재 몸무게의 제곱 - 기억 하고 있던 몸무게의 제곱
-        List<Integer> arr = new ArrayList<>();
-
+        StringBuilder sb = new StringBuilder();
+        
         int s = 1;
         int e = 1;
-
-        StringBuilder sb = new StringBuilder();
-        boolean flag = false;
-        while (s <= e && e <= 100000) {
-            int gap = e * e - s * s;
-            if (gap == g) {
-                flag = true;
+        while (e <= 50000) {
+            if (e * e - s * s == g) {
                 sb.append(e).append("\n");
-                e++;
-            } else if (gap > g) {
                 s++;
-            } else {
                 e++;
+            } else if (e * e - s * s < g) {
+                e++;
+            } else {
+                s++;
             }
         }
-        if (flag) {
-            System.out.println(sb);
-        } else {
-            System.out.println("-1");
+
+        if (sb.length() == 0) {
+            System.out.println(-1);
+            return;
         }
+        System.out.println(sb);
     }
 }
