49640107d67f8a996f6d93faf8cf0e0312b57dba
doxxx <doxxx93@gmail.com>
2023-02-14 14:53:44 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 804 ms, Memory: 14936 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
@@ -5,49 +5,41 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
         Arrays.sort(arr);
 
         int answer = Integer.MAX_VALUE;
 
-        for (int es = 0; es < n; es++) {
-            for (int ee = n - 1; ee > es; ee--) {
-                int result = arr[es] + arr[ee];
-                int as = 0;
-                int ae = n - 1;
+        for (int as = 0; as < n; as++) {
+            for (int ae = n - 1; ae > as; ae--) {
+                int anna = arr[as] + arr[ae];
+                int es = 0;
+                int ee = n - 1;
 
-                while (as < ae) {
-                    if (as == es || as == ee) {
-                        as++;
+                while (es < ee) {
+                    if (es == as || es == ae) {
+                        es++;
                         continue;
                     }
-                    if (ae == ee || ae == es) {
-                        ae--;
+                    if (ee == ae || ee == as) {
+                        ee--;
                         continue;
                     }
-                    int res = arr[as] + arr[ae];
-                    answer = Math.min(answer, Math.abs(result - res));
+                    int elsa = arr[es] + arr[ee];
+                    answer = Math.min(answer, Math.abs(anna - elsa));
 
-                    if (result < res) {
-                        ae--;
-                    } else if (result > res) {
-                        as++;
+                    if (anna > elsa) {
+                        es++;
+                    } else if (anna < elsa) {
+                        ee--;
                     } else {
                         break;
                     }
                 }
             }
         }
-        bw.write(String.valueOf(answer));
-        bw.flush();
-        bw.close();
+        System.out.println(answer);
     }
 }
