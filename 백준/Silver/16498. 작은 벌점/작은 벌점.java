a675a964db7316fc899f8740263981294854b20e
doxxx <doxxx93@gmail.com>
2023-03-05 16:28:41 +0900

    [Silver I] Title: 작은 벌점, Time: 332 ms, Memory: 20332 KB -BaekjoonHub

--- 백준/Silver/16498. 작은 벌점/작은 벌점.java
+++ 백준/Silver/16498. 작은 벌점/작은 벌점.java
@@ -1,32 +1,44 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[][] arr = new int[3][];
-        for (int i = 0; i < 3; i++) {
-            arr[i] = new int[Integer.parseInt(st.nextToken())];
-        }
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
+        List<Integer>[] list = new ArrayList[3];
         for (int i = 0; i < 3; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+            list[i] = new ArrayList<>();
+            Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt)
+                .forEach(list[i]::add);
+            Collections.sort(list[i]);
         }
         int answer = Integer.MAX_VALUE;
-        for (int i = 0; i < arr[0].length; i++) {
-            for (int j = 0; j < arr[1].length; j++) {
-                for (int k = 0; k < arr[2].length; k++) {
-                    answer = Math.min(answer, Math.abs(
-                        Math.max(Math.max(arr[0][i], arr[1][j]), arr[2][k]) - Math.min(
-                            Math.min(arr[0][i], arr[1][j]), arr[2][k])));
+        for (int x : list[0]) {
+            for (int y : list[1]) {
+                //upperbound min (x,y) in list[2]
+                int index = Collections.binarySearch(list[2], Math.min(x, y));
+                
+                if (index < 0) {
+                    index = -index - 1;
+                }
+                if (index < list[2].size()) {
+                    answer = Math.min(answer,
+                        Math.max(x, Math.max(y, list[2].get(index))) - Math.min(x,
+                            Math.min(y, list[2].get(index))));
+                }
+                if (index > 0) {
+                    answer = Math.min(answer,
+                        Math.max(x, Math.max(y, list[2].get(index - 1))) - Math.min(x,
+                            Math.min(y, list[2].get(index - 1))));
                 }
             }
         }
+
         System.out.println(answer);
     }
 }
