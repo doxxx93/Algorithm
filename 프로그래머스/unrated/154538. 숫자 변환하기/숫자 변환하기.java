fad529b921453fae174d0ef3a6a542c4e7eb16ee
doxxx <doxxx93@gmail.com>
2023-03-10 10:54:34 +0900

    [unrated] Title: 숫자 변환하기, Time: 1.55 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
@@ -5,9 +5,31 @@ class Solution {
     public int solution(int x, int y, int n) {
         int count = 0;
         Queue<Integer> queue = new LinkedList<>();
-        Set<Integer> visited = new TreeSet<>();
         queue.add(x);
-        visited.add(x);
+        boolean[] visited = new boolean[y + 1];
+        visited[x] = true;
+//        while (!queue.isEmpty()) {
+//            int size = queue.size();
+//            for (int i = 0; i < size; i++) {
+//                int cur = queue.poll();
+//                if (cur == y) {
+//                    return count;
+//                }
+//                if (cur + n <= y && !visited.contains(cur + n)) {
+//                    queue.add(cur + n);
+//                    visited.add(cur + n);
+//                }
+//                if (cur * 2 <= y && !visited.contains(cur * 2)) {
+//                    queue.add(cur * 2);
+//                    visited.add(cur * 2);
+//                }
+//                if (cur * 3 <= y && !visited.contains(cur * 3)) {
+//                    queue.add(cur * 3);
+//                    visited.add(cur * 3);
+//                }
+//            }
+//            count++;
+//        }
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
@@ -15,17 +37,17 @@ class Solution {
                 if (cur == y) {
                     return count;
                 }
-                if (cur + n <= y && !visited.contains(cur + n)) {
+                if (cur + n <= y && !visited[cur + n]) {
                     queue.add(cur + n);
-                    visited.add(cur + n);
+                    visited[cur + n] = true;
                 }
-                if (cur * 2 <= y && !visited.contains(cur * 2)) {
+                if (cur * 2 <= y && !visited[cur * 2]) {
                     queue.add(cur * 2);
-                    visited.add(cur * 2);
+                    visited[cur * 2] = true;
                 }
-                if (cur * 3 <= y && !visited.contains(cur * 3)) {
+                if (cur * 3 <= y && !visited[cur * 3]) {
                     queue.add(cur * 3);
-                    visited.add(cur * 3);
+                    visited[cur * 3] = true;
                 }
             }
             count++;
