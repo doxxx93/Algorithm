8bd684da4e25781f07a2d8a7cc36419a80979765
doxxx <doxxx93@gmail.com>
2023-03-11 21:00:36 +0900

    [level 4] Title: 사칙연산, Time: 22.70 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/lv4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/lv4/1843. 사칙연산/사칙연산.java
@@ -3,27 +3,34 @@ import java.util.*;
 class Solution {
 
     public int solution(String[] arr) {
-        int op = (arr.length - 1) / 2;
-        int[][] max = new int[op + 1][op + 1];
-        int[][] min = new int[op + 1][op + 1];
-        for (int i = 0; i < op + 1; i++) {
+        List<Integer> nums = new ArrayList<>();
+        List<String> ops = new ArrayList<>();
+        Arrays.stream(arr).forEach(s -> {
+            if (s.equals("+") || s.equals("-")) {
+                ops.add(s);
+            } else {
+                nums.add(Integer.parseInt(s));
+            }
+        });
+        int[][] max = new int[nums.size()][nums.size()];
+        int[][] min = new int[nums.size()][nums.size()];
+        for (int i = 0; i < nums.size(); i++) {
             Arrays.fill(max[i], Integer.MIN_VALUE);
             Arrays.fill(min[i], Integer.MAX_VALUE);
         }
-        for (int i = 0; i < op + 1; i++) {
-            max[i][i] = Integer.parseInt(arr[i * 2]);
-            min[i][i] = Integer.parseInt(arr[i * 2]);
+        for (int i = 0; i < nums.size(); i++) {
+            max[i][i] = nums.get(i);
+            min[i][i] = nums.get(i);
         }
 
-        // operators are + , -
-        for (int i = 1; i < op + 1; i++) {
-            for (int j = 0; j < op + 1 - i; j++) {
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size() - i; j++) {
                 for (int k = j; k < j + i; k++) {
                     int a = max[j][k];
                     int b = max[k + 1][j + i];
                     int c = min[j][k];
                     int d = min[k + 1][j + i];
-                    if (arr[k * 2 + 1].equals("+")) {
+                    if (ops.get(k).equals("+")) {
                         max[j][j + i] = Math.max(max[j][j + i], a + b);
                         min[j][j + i] = Math.min(min[j][j + i], c + d);
                     } else {
@@ -33,6 +40,6 @@ class Solution {
                 }
             }
         }
-        return max[0][op];
+        return max[0][nums.size() - 1];
     }
 }
