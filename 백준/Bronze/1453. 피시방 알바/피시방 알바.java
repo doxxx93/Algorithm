77f7a4a5093fc21cbfb99f949ba094b25a9673d5
doxxx <doxxx93@gmail.com>
2023-04-04 21:30:37 +0900

    [Bronze II] Title: 피시방 알바, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/1453. 피시방 알바/피시방 알바.java
@@ -0,0 +1,22 @@
+import java.io. *;
+import java.util. *;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+        boolean[] check = new boolean[101];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int seat = Integer.parseInt(st.nextToken());
+            if (check[seat]) {
+                count++;
+            } else {
+                check[seat] = true;
+            }
+        }
+        System.out.println(count);
+    }
+}
