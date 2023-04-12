3d17b94adb0200a34b9a339aa92d69a1e9d72f46
doxxx <doxxx93@gmail.com>
2023-04-12 23:58:31 +0900

    [Bronze II] Title: 치킨 쿠폰, Time: 128 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/1673. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    static public void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+            String line = br.readLine();
+
+            if(line == null) break;
+
+            String[] str = line.split(" ");
+
+            int coupon = Integer.parseInt(str[0]);
+            int k = Integer.parseInt(str[1]);
+
+            int count =0;
+            int newCoupon;
+            int newCount;
+
+            while(true){
+                newCount = (coupon - coupon%k);
+                newCoupon = newCount/k;
+                coupon = coupon%k;
+                coupon += newCoupon;
+
+                count += newCount;
+                if(coupon < k) {
+                    count += coupon;
+                    break;
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+}
