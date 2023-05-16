8b7aee3497a3fd7bd69b9281ea3bbd48fe449b79
doxxx <doxxx93@gmail.com>
2023-05-16 17:46:48 +0900

    [Gold IV] Title: 트리의 지름, Time: 232 ms, Memory: 22064 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -4,42 +4,10 @@ import java.util.*;
 public class Main {
 
     static int n;
-    static ArrayList<Node>[] graph;
-    static int distance;
-    static boolean[] visited;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        graph = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        for (int i = 0; i < n - 1; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            graph[a].add(new Node(b, c));
-            graph[b].add(new Node(a, c));
-        }
-        distance = 0;
-        for (int i = 1; i <= n; i++) {
-            visited = new boolean[n + 1];
-            visited[i] = true;
-            dfs(i, 0);
-        }
-        System.out.println(distance);
-    }
-    public static void dfs(int start, int sum) {
-        for (Node node : graph[start]) {
-            if (!visited[node.to]) {
-                visited[node.to] = true;
-                dfs(node.to, sum + node.weight);
-            }
-        }
-        distance = Math.max(distance, sum);
-    }
+    private static List<Node>[] tree;
+    private static int end;
+    private static int max;
+    private static boolean[] visited;
 
     static class Node {
 
@@ -49,6 +17,48 @@ public class Main {
         public Node(int to, int weight) {
             this.to = to;
             this.weight = weight;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+
+        tree = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            tree[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int parent = Integer.parseInt(st.nextToken());
+            int child = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            tree[parent].add(new Node(child, weight));
+            tree[child].add(new Node(parent, weight));
+        }
+
+        visited = new boolean[n + 1];
+        max = Integer.MIN_VALUE;
+        dfs(1, 0);
+
+        visited = new boolean[n + 1];
+        max = Integer.MIN_VALUE;
+        dfs(end, 0);
+        System.out.println(max);
+    }
+
+    private static void dfs(int start, int weight) {
+        visited[start] = true;
+        if (weight > max) {
+            max = weight;
+            end = start;
+        }
+        for (Node node : tree[start]) {
+            if (!visited[node.to]) {
+                dfs(node.to, weight + node.weight);
+            }
         }
     }
 }
