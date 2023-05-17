063c8191fe4b05cf4668c2f9a41c76c1ec3b6ef6
doxxx <doxxx93@gmail.com>
2023-05-17 14:48:08 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] array) {
+        int count = 0;
+        for(int x : array){
+            int tmp = 0;
+            while(x > 0){
+                if (x%10 == 7){
+                    tmp++;
+                }
+                x /= 10;
+            }
+            count += tmp;
+        }
+        return count;
+    }
+}
