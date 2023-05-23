712de69d6260407cfc5094e7df6e92d9e9cae998
doxxx <doxxx93@gmail.com>
2023-05-23 20:06:26 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.71 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -1,23 +1,36 @@
+import java.math.*;
 import java.util.*;
 
 class Solution {
-    static int[][] cache = new int[31][31];
+//
+//    static int[][] dp = new int[31][31];
+//
+//    public int solution(int balls, int share) {
+//        for (int[] ints : dp) {
+//            Arrays.fill(ints, -1);
+//        }
+//        return combination(balls, share);
+//    }
+//
+//    int combination(int n, int r) {
+//        if (r == 0 || r == n) {
+//            return 1;
+//        }
+//
+//        if (dp[n][r] != -1) {
+//            return dp[n][r];
+//        }
+//
+//        return dp[n][r] = combination(n - 1, r - 1) + combination(n - 1, r);
+//    }
+    
+    // use BigInteger
     public int solution(int balls, int share) {
-        for(int i = 0; i< cache.length;i++){
-            Arrays.fill(cache[i], -1);
+        BigInteger answer = BigInteger.ONE;
+        for (int i = 0; i < share; i++) {
+            answer = answer.multiply(BigInteger.valueOf(balls - i)).divide(BigInteger.valueOf(i + 1));
         }
-        return c(balls, share);
+        return answer.intValue();
     }
     
-    int c(int n, int r){
-        if(r==0 || r ==n){
-            return 1;
-        }
-        
-        if (cache[n][r] != -1){
-            return cache[n][r];
-        }
-        
-        return cache[n][r] = c(n-1, r-1) + c(n-1,r);
-    }
 }
