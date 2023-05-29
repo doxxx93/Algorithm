ac5530fda14ad6da1926b56c719e6eba0603885f
doxxx <doxxx93@gmail.com>
2023-05-29 23:13:23 +0900

    [Bronze V] Title: Can you add this?, Time: 128 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+        }
+    }
+}
