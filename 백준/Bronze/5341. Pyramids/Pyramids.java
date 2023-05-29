158bf0d165e264ae6b5064c2fcb4c07899aa43d6
doxxx <doxxx93@gmail.com>
2023-05-30 02:07:49 +0900

    [Bronze V] Title: Pyramids, Time: 120 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/5341. Pyramids/Pyramids.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) {
+                break;
+            }
+            System.out.println(n * (n + 1) / 2);
+        }
+    }
+}
