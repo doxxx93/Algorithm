e722b7c2ca432cd10d6b4932695e8ca2e561b4cd
doxxx <doxxx93@gmail.com>
2023-07-22 17:10:55 +0900

    [Bronze II] Title: 지수연산, Time: 0 ms, Memory: 13156 KB -BaekjoonHub

---
+++ 백준/Bronze/2052. 지수연산/지수연산.rs
@@ -0,0 +1,54 @@
+use std::fmt;
+
+const DIGITS: usize = 37;
+const POW: i128 = 10_i128.pow(DIGITS as u32);
+
+struct BigInt(Vec<i128>);
+
+impl BigInt {
+    fn mul_assign(&mut self, other: i128) {
+        let mut carry = 0;
+
+        for num in &mut self.0 {
+            let temp = carry + *num * other;
+
+            carry = temp / POW;
+            *num = temp % POW;
+        }
+
+        if carry > 0 {
+            self.0.push(carry);
+        }
+    }
+}
+
+impl fmt::Display for BigInt {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for (i, num) in self.0.iter().rev().enumerate() {
+            if i == 0 {
+                write!(f, "{num}")
+            } else {
+                write!(f, "{num:0DIGITS$}")
+            }
+            .unwrap();
+        }
+
+        Ok(())
+    }
+}
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
+    let n: usize = buf.trim().parse().unwrap();
+    let mut bigint = BigInt(vec![1]);
+
+    for _ in 0..n {
+        bigint.mul_assign(5);
+    }
+
+    let result = bigint.to_string();
+
+    println!("0.{}{result}", "0".repeat(n - result.len()));
+}
