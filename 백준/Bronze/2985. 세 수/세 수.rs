0b084e38d30d3c616aaa2e900de774f8e8181d5c
doxxx <doxxx93@gmail.com>
2023-07-22 15:30:15 +0900

    [Bronze III] Title: 세 수, Time: 4 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.rs
@@ -0,0 +1,28 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let mut iter = input.split_whitespace();
+    let a: i32 = iter.next().unwrap().parse().unwrap();
+    let b: i32 = iter.next().unwrap().parse().unwrap();
+    let c: i32 = iter.next().unwrap().parse().unwrap();
+
+    if a + b == c {
+        println!("{}+{}={}", a, b, c);
+    } else if a - b == c {
+        println!("{}-{}={}", a, b, c);
+    } else if a * b == c {
+        println!("{}*{}={}", a, b, c);
+    } else if a / b == c {
+        println!("{}/{}={}", a, b, c);
+    } else if a == b + c {
+        println!("{}={}+{}", a, b, c);
+    } else if a == b - c {
+        println!("{}={}-{}", a, b, c);
+    } else if a == b * c {
+        println!("{}={}*{}", a, b, c);
+    } else if a == b / c {
+        println!("{}={}/{}", a, b, c);
+    }
+}
