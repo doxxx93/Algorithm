d546ebb6647dc62aea32fd66d43895a5843eca31
doxxx <doxxx93@gmail.com>
2024-04-20 15:01:38 +0900

    Time: 14 ms (55.89%), Space: 72.8 MB (11.79%) - LeetHub

--- LeetCode/1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
+++ LeetCode/1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
 
     private static final int[] dx = {0, 1, 0, -1};
@@ -11,9 +8,9 @@ class Solution {
         for (int i = 0; i< land.length; i++){
             for (int j = 0; j < land[0].length; j++){
                 if (land[i][j] == 1) {
-                    int[] farm = {i, j, i, j};
-                    dfs(land, i, j, farm);
-                    res.add(farm);
+                    int[] lect = {i, j, i, j};
+                    dfs(land, i, j, lect);
+                    res.add(lect);
                 }
             }
         }
@@ -21,19 +18,19 @@ class Solution {
         return res.toArray(new int[0][]);
     }
 
-    private void dfs(int[][] land, int i, int j, int[] farm){
+    private void dfs(int[][] land, int i, int j, int[] lect){
         if (i < 0 || i >= land.length || j < 0 || j >= land[0].length || land[i][j] == 0){
             return;
         }
         land[i][j] = 0;
-        farm[0] = Math.min(farm[0], i);
-        farm[1] = Math.min(farm[1], j);
-        farm[2] = Math.max(farm[2], i);
-        farm[3] = Math.max(farm[3], j);
+        lect[0] = Math.min(lect[0], i);
+        lect[1] = Math.min(lect[1], j);
+        lect[2] = Math.max(lect[2], i);
+        lect[3] = Math.max(lect[3], j);
         for (int k = 0; k < 4; k++){
             int x = i + dx[k];
             int y = j + dy[k];
-            dfs(land, x, y, farm);
+            dfs(land, x, y, lect);
         }
     }
 }
