7cbeb7ec506c4229feb6d4f2e8635dd266ade344
doxxx <doxxx93@gmail.com>
2024-06-03 15:01:03 +0900

    Time: 7 ms (51.85%), Space: 45.8 MB (7.91%) - LeetHub

---
+++ LeetCode/2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int si = 0;
+        int ti = 0;
+
+        while (si < s.length() && ti < t.length()) {
+            if (s.charAt(si) == t.charAt(ti)) {
+                ti++;
+            }
+            si++;
+        }
+
+        return t.length() - ti;
+    }
+}
